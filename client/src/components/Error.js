75845e029a3b5df55ad5c03dc1c472420939879a
Will-Copeland <will@localhost.localdomain>
2019-02-28 21:33:57 -0800

    "

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import {
-  withStyles, Snackbar, Button, IconButton,
+  withStyles, Snackbar,
 } from '@material-ui/core';
 
-const styles = theme => ({
-  error: {
-    backgroundColor: theme.palette.error.main,
-  }
- 
-});
-
-const Error = ({ classes, error, open, handleClose }) => (
-  <div >
+const Error = ({ error, open, handleClose }) => (
+  <div>
     <Snackbar
       anchorOrigin={{
         vertical: 'bottom',
@@ -21,7 +14,6 @@ const Error = ({ classes, error, open, handleClose }) => (
       open={open}
       autoHideDuration={2000}
       ContentProps={{
-        classes: classes.error,
         'aria-describedby': 'message-id',
       }}
       message={<span id="message-id">{error}</span>}
